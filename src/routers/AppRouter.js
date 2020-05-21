d8218ed2529fce07f0d822ef98f5a108dcda9101
Matias Fessia <matiasfessia@gmail.com>
2020-05-21 10:39:30 -0300

    several changes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,19 +7,22 @@ import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFoundPage';
 import Header from '../components/Header';
 
-const AppRouter = () => (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const AppRouter = () => {
+  console.log('-----------------')
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/" component={ExpenseDashboardPage} exact={true} />
+          <Route path="/create" component={AddExpensePage} />
+          <Route path="/edit/:id" component={EditExpensePage} />
+          <Route path="/help" component={HelpPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default AppRouter;
